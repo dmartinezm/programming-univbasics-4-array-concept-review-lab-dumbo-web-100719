99ee12e66920a1354226cc0326166f7747c4327e
Diego Martinez <dmartinezm@gmail.com>
2019-09-26 03:02:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.include?(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max_by{|array| array}
 end
 
 def find_min_value(array)
